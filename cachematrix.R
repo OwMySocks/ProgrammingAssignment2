a3b074eb94bc6cdcab9d8b5b6087465bdc1fa4ba
Bexett <rebecca.everett.7@gmail.com>
2014-09-16 21:29:35 -0500

    Added examples to play with and began cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This creates a matrix object with the ability to solve
+## and cache the inverse of this object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## check if matrix is invertible
+  if (det(x) == 0) stop
+  ## instantiate inverse
+  inv <- NULL
+  
 }
 
 
