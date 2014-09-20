30ba97248b6fbcfc669b6e5942c65d61ca502dbf
Bexett <rebecca.everett.7@gmail.com>
2014-09-20 13:06:42 -0500

    Final revisions with better comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,9 @@
 ## But I dont know what they do, it doesn't make sense yet
 ## This creates a matrix object with the ability to solve
 ## and cache the inverse of this object. It mostly just sets up and
-## holds things
-## in little pocket universes 
+## holds things in little pocket universes
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## check if matrix is invertible
-  ##if (det(x) == 0) stop
   ## instantiate inverse. I think this is probably a good idea?
   inv <- NULL
   
@@ -18,10 +15,10 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   
-  ##get function returns matrix
+  ##get function returns main matrix
   get <- function() x
   
-  ##set function to set the inverse- not sure if I need this
+  ##set function to set the inverse, used by cacheSolve
   setinv <- function(inverse) inv <<- inverse 
   
   ##getinv function returns inverse
@@ -35,19 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  #print("Hello?")
-  ## Return a matrix that is the inverse of 'x'
+  ## Return what currently exists as inv in the other function
   inv <- x$getinv()
-  ##print("got inverse")
-  ## if that gave us something, then pull it out from the banished 
-  ## pocket dimension and spit it out
+  
+  ## if that gave us something, then return it
   if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  ## on the other hand
-  ## get the original matrix from the new makeCacheMatrix
-  ## pocket universe where I tucked it
+  
+  ## if not, pull the original matrix from cache and solve for the
+  ## inverse, tucking it into the cache if so.
   message("did not find a cached inverse")
   data <- x$get()
   inverse <- solve(data)
